c1864afe039e5cb8c4e487d6d4a6c28680ed541d
marianaoliveirafernandes@alu.ufc.br <marianaoliveirafernandes@alu.ufc.br>
2022-04-12 20:37:37 -0300

    #2

---
+++ fundamentos-react1/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { Primeiro } from "./components/Primeiro";
+import './index.css'
+
+ReactDOM.render(
+    <Primeiro />,
+    document.getElementById('root')
+)
